e8e2c0484a088a67bef3e30904546618ac0e7371
Himanshu Uppal <himanshu.uppal@nagarro.com>
2018-10-27 13:00:38 +0530

    registered a user and saved it in db

--- routes/users.js
+++ routes/users.js
@@ -1,15 +1,28 @@
 const {Router} = require('express')
+const {User} = require('../models')
 
 const router = Router()
 
-router.get('/',(req,res)=>{
+router.get('/',async (req,res)=>{
     console.log('users fetched')
     res.send('users fetched')
 })
 
-router.post('/',(req,res)=>{
+router.post('/',async (req,res)=>{
+    let user = new User()
     console.log('users created')
-    res.send('users created')
+    if(req.body){
+        user.username = req.body.username
+        user.email = req.body.email
+        user.password = req.body.password
+        console.log('body has data')
+        user.save().then(()=>{
+            res.json(user)
+            console.log('user saved')
+        })
+        
+    }    
 })
+
 
 module.exports = router
